bbdf0d4f46078427b2ede73e21782fef24c4dd36
tahmidk <tahmidkhan3141@gmail.com>
2017-11-19 00:51:45 -0800

    Implemented queen's moves
    Implemented queen's moves and animations
    Began implementing a check/checkmate system

--- Modern Chess/Assets/Scripts/Board Scripts/EmptyBoardObject.cs
+++ Modern Chess/Assets/Scripts/Board Scripts/EmptyBoardObject.cs
@@ -8,8 +8,87 @@ using UnityEngine;
  */
 public class EmptyBoardObject : BoardObjects
 {
+    /* The [status] component implemented for the EmptyBoardObjects is pivotal to the implementation of
+     * the check/checkmate system. The following is what each status denots:
+     * -----Status------|------------------------Description-------------------------
+     * FREE             | this space is not currently in the moveset of any of the chesspieces
+     * WHITE_OCCUPIED   | this space is in the moveset of at least 1 white piece
+     * BLACK_OCCUPIED   | this space is in the moveset of at least 1 black piece
+     * BOTH_OCCUPIED    | this space is in the moveset of at least 1 piece from both teams
+     */
+    public enum Status {FREE, NULL, WHITE_OCCUPIED, BLACK_OCCUPIED, BOTH_OCCUPIED}
+
+    public Status status;   /* Holds the occupation status of this empty space */
+
     public EmptyBoardObject()
     {
         ObjType = BoardObjects.Type.EMPTY;
+        status = Status.FREE;
+    }
+
+    /** Function:   RemoveStatus(Status)
+     *  Argument:   s - the status to remove
+     *  Output:     Removes the status s from this object and appropriately updates the status
+     *              field to accurately reflect the change
+     */
+    public void RemoveStatus(Status s)
+    {
+        switch(status)
+        {
+            // If [status] is already FREE, RemoveStatus changes nothing regardless of s
+            case Status.FREE:           
+                break;
+            // If [status] is WHITE_OCCUPIED and we're trying to remove the status s = WHITE_OCCUPIED, the new 
+            // [status] is FREE. Otherwise, nothing changes
+            case Status.WHITE_OCCUPIED:
+                if (s == Status.WHITE_OCCUPIED)
+                    status = Status.FREE;
+                break;
+            // The black analog to the above case
+            case Status.BLACK_OCCUPIED:
+                if (s == Status.BLACK_OCCUPIED)
+                    status = Status.FREE;
+                break;
+            // If [status] is BOTH_OCCUPIED, it becomes FREE if we remove the status BOTH_OCCUPIED
+            //                              ... becomes WHITE_OCCUPIED if we remove the BLACK_OCCUPIED status
+            //                              ... becomes BLACK_OCCUPIED if we remove the WHITE_OCCUPIED status
+            case Status.BOTH_OCCUPIED:
+                if (s == Status.BOTH_OCCUPIED)
+                    status = Status.FREE;
+                else if (s == Status.BLACK_OCCUPIED)
+                    status = Status.WHITE_OCCUPIED;
+                else if (s == Status.WHITE_OCCUPIED)
+                    status = Status.BLACK_OCCUPIED;
+                break;
+        }
+    }
+
+    /** Function:   UpdateStatus(Status)
+     *  Argument:   s - the status to add
+     *  Output:     Updates the [status] field such that it appropriately includes the status s
+     */
+    public void UpdateStatus(Status s)
+    {
+        switch(status)
+        {
+            // Update normally if [status] field is FREE
+            case Status.FREE:
+                status = s;
+                break;
+            // If [status] field is WHITE_OCCUPIED and s is BLACK_OCCUPIED, [status] becomes both occupied.
+            // Otherwise, if s is WHITE_OCCUPIED, nothing changes
+            case Status.WHITE_OCCUPIED:
+                if (s == Status.BLACK_OCCUPIED)
+                    status = Status.BOTH_OCCUPIED;
+                break;
+            // The black analog to the above case
+            case Status.BLACK_OCCUPIED:
+                if (s == Status.WHITE_OCCUPIED)
+                    status = Status.BOTH_OCCUPIED;
+                break;
+            // If [status] is BOTH_OCCUPIED, UpdateStatus() does not change anything regardless of s
+            case Status.BOTH_OCCUPIED:
+                break;
+        }
     }
 }
--- Modern Chess/Assets/Scripts/Piece Scripts/Queen.cs
+++ Modern Chess/Assets/Scripts/Piece Scripts/Queen.cs
@@ -8,20 +8,82 @@ public class Queen : Chesspiece
 
     private void Start()
     {
-        
+        animator = this.GetComponent<Animator>();
+        role = Rank.QUEEN;
+        destination = this.transform.position;
     }
 
     private void Update()
     {
-        
+        // If there is a new destination, move towards that destination
+        if (transform.position != destination)
+        {
+            float distance = Vector3.Distance(transform.position, destination);
+            if (distance > 3)
+                animator.Play("Queen_SLIDE", -1, 0f);
+            else
+                animator.Play("Queen_SLIDE_STOP", -1, 0f);
+
+            Vector3 start_pos = this.GetComponentInParent<Transform>().position;
+            Vector3 direction = destination - start_pos;
+            transform.rotation = Quaternion.LookRotation(-1 * direction);
+            transform.position = Vector3.MoveTowards(start_pos, destination, BASE_SPEED * Time.deltaTime);
+        }
     }
 
-    public override void GoToSlide(Vector3 destination)
+    /** Function:   PossibleMoves (Queen Override)
+     *  Argument:   None
+     *  Output:     Returns the possible moves of a rook. A queen can normally attack in any of the 8 directions
+     *              unrestrictedly. This implementation is based on the fact that a queen has the movability
+     *              of both a rook and bishop at the same position 
+     */
+    public override Hashtable PossibleMoves()
     {
-        
+        GameObject temp = new GameObject();
+        temp.AddComponent<Rook>();
+        temp.AddComponent<Bishop>();
+
+        // Simulate a rook's moves if it were at this queen's position
+        Rook tempRook = temp.GetComponent<Rook>();
+        tempRook.SetPosition(this.CurrentX, this.CurrentY);
+        tempRook.isWhite = this.isWhite;
+        Hashtable vertMoves = tempRook.PossibleMoves();
+
+        // Simulate bishop's moves if it were at this queen's position
+        Bishop tempBishop = temp.GetComponent<Bishop>();
+        tempBishop.SetPosition(this.CurrentX, this.CurrentY);
+        tempBishop.isWhite = this.isWhite;
+        Hashtable diagMoves = tempBishop.PossibleMoves();
+
+        // Union the above movesets to get the queen's moveset. There will be no
+        // Overlap issues as the horizontal and diagonal moves don't intersect
+        Hashtable moveset = new Hashtable();
+        foreach (DictionaryEntry entry in vertMoves)
+            moveset.Add(entry.Key, entry.Value);
+        foreach (DictionaryEntry entry in diagMoves)
+            moveset.Add(entry.Key, entry.Value);
+
+        return moveset;
     }
 
-    public override void GoToAttack(Vector3 destination)
+    /** Function:   GoToSlide(Vec3)
+     *  Argument:   Vec3 dest - the exact world coordinate to jump to (NOT board coordinates)
+     *  Output:     Executes an animated slide to the position given by the vector dest with 
+     *              the appropriate animations
+     */
+    public override void GoToSlide(Vector3 dest)
+    {
+        if (dest == destination)
+            return;
+
+        // Begin slide animation
+        animator.Play("Queen_SLIDE_BEGIN", -1, 0f);
+
+        // Transition to new position
+        StartCoroutine(DelayedTransition(dest));
+    }
+
+    public override void GoToAttack(Vector3 dest)
     {
         
     }
